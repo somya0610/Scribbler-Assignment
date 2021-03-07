c3791d80156228dee03d6040236fb91720bda9fe
Somya <somya0610@gmail.com>
2021-03-07 11:57:39 -0800

    Home Page - Feature a.2 - On clicking the 'Sign Up' button, a modal appears on the screen that consists of 4 input boxes and a button. Funtionality added to the modal as per the instruction

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,7 @@
 /** Since header is common in all the html pages, we create header content in common.js, so that code can be reused */
 
-/**Create header with title of the website as "ScriBBler", the subheading "Explore, Imagine, Create" on load of the screens*/
+/**Function to create header with title of the website as "ScriBBler",
+ *  the subheading "Explore, Imagine, Create" on load of the screen*/
 onLoadCreateHeader();
 function onLoadCreateHeader() {
     var template = "<div>" +
@@ -14,3 +15,62 @@ function onLoadCreateHeader() {
 
     document.getElementsByTagName('header')[0].innerHTML += template;
 }
+
+/** Function to create Sign Up Modal on load of the screen */
+onLoadCreateSignUpModal();
+function onLoadCreateSignUpModal() {
+    var template = "<div id=\"sign-up-modal\" class=\"modal\">" +
+        "<div class=\"modal-content\">" +
+        "<div class=\"modal-header\">" +
+        "<p>Get Started</p>" +
+        "<span class=\"modal-close\"><i class=\"fa fa-times\" aria-hidden=\"true\"></i></span>" +
+        "</div>" +
+        "<hr>" +
+        "<form>" +
+        "<label for=\"name\">Name</label>" +
+        "<input type=\"text\" id=\"name\" placeholder=\"Enter your name\" required autofocus>" +
+        "<label for=\"username\">Username</label>" +
+        "<input type=\"text\" id=\"username\" placeholder=\"Enter your username\" required autofocus>" +
+        "<label for=\"password\">Password</label>" +
+        "<input type=\"password\" id=\"password\" placeholder=\"Enter your password\" required autofocus>" +
+        "<label for=\"confirm-password\">Confirm Password</label>" +
+        "<input type=\"password\" id=\"confirm-password\" placeholder=\"Re-enter your password\" required autofocus>" +
+        "<button class=\"modal-button\">Sign Up</button>" +
+        "</form>" +
+        "</div>" +
+        "</div>";
+
+    document.getElementsByTagName('header')[0].innerHTML += template;
+}
+
+/** Below code opens the Sign Up modal */
+
+var signUpModal = document.getElementById("sign-up-modal"); // Get the div element for Sign Up Modal
+var signUpButton = document.getElementById("sign-up"); // Get the button that opens the Sign Up modal
+var signUpSpan = document.getElementsByClassName("modal-close")[0]; // Get the <span> element that closes the Sign Up Modal
+
+// On clicking Sign Up button, Sign Up modal appears on the screen
+signUpButton.onclick = function () {
+    signUpModal.style.display = "block";
+}
+
+// When the user clicks on Sign Up <span> (x), close the Sign Up Modal
+signUpSpan.onclick = function () {
+    setModalToDefault(signUpModal);
+}
+
+/** Funtion to set the style display as 'none' and all input fields to default placeholder values for modal */
+function setModalToDefault(modalElement) {
+    modalElement.style.display = "none";
+    var inputTagArray = modalElement.getElementsByTagName('input');
+    for (var index = 0; index < inputTagArray.length; index++) {
+        inputTagArray[index].value = '';
+    }
+}
+
+// When the user clicks anywhere outside of the Sign Up Modal, the modal is closed
+window.addEventListener('click', function (event) {
+    if (event.target == signUpModal) {
+        setModalToDefault(signUpModal);
+    }
+})
